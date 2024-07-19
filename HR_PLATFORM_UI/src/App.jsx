820c90d091253f80beb3aefcb30613ea927a9888
Catal1nPopa <misterco2002@gmail.com>
2024-07-19 15:35:21 +0300

    Recreated project

--- HR_PLATFORM_UI/src/App.jsx
+++ HR_PLATFORM_UI/src/App.jsx
@@ -13,7 +13,15 @@ import {
   CardContent,
   CardMedia,
   Typography,
+  Box,
+  Button,
 } from "@mui/material";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import HomePage from "./Components/Main/MainPage";
+import LoginPage from "./Components/Auth/LoginPage";
+
+import darkBackground from "./assets/black_mode.jpg";
+import lightBackground from "./assets/white_mode.jpg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -29,9 +37,39 @@ function App() {
       mode: toggleDarkMode ? "dark" : "light", //default theme
       primary: {
         main: "#90caf9",
+        light: "#e3f2fd",
+        dark: "#42a5f5",
+        contrastText: "rgba(0,0,0,0.87)",
       },
       secondary: {
-        main: "#f48fb1",
+        main: "#ce93db",
+        light: "#f3e5f5",
+        dark: "#ab47bc",
+        contrastText: "rgba(0,0,0,0.87)",
+      },
+      error: {
+        main: "#f44336",
+        light: "#e57373",
+        dark: "#d32f2f",
+        contrastText: "#fff",
+      },
+      warning: {
+        main: "#ffa726",
+        light: "#ffb47d",
+        dark: "#f57c00",
+        contrastText: "rgba(0,0,0,0.87)",
+      },
+      info: {
+        main: "#29b6f6",
+        light: "#4fc3f7",
+        dark: "#0288d1",
+        contrastText: "rgba(0,0,0,0.87)",
+      },
+      success: {
+        main: "#66bb6a",
+        light: "#81c784",
+        dark: "#388e3c",
+        contrastText: "rgba(0,0,0,0.87)",
       },
     },
   });
@@ -39,38 +77,15 @@ function App() {
   return (
     <>
       <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <h2>toggle dark mode</h2>
-          <Switch checked={toggleDarkMode} onChange={toggleDarkTheme} />
-          <Card sx={{ width: "30%", borderRadius: 3, padding: 1 }}>
-            <CardContent>
-              <CardMedia
-                sx={{ height: 180, borderRadius: 3 }}
-                image='https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg'
-                title='semaphore'
-              />
-              <Typography variant='h4' component='div' sx={{ marginTop: 3 }}>
-                <button>{t("Logout")}</button> <button>{t("Login")}</button>
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                by Semaphore
-              </Typography>
-              <Typography variant='body1'>{t("Description")}</Typography>
-              <SwitchLanguage />
-            </CardContent>
-          </Card>
-        </div>
+        {/* <CssBaseline /> */}
+        {/* <Switch checked={toggleDarkMode} onChange={toggleDarkTheme} /> */}
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
+        <SwitchLanguage />
       </ThemeProvider>
     </>
-    //    <SwitchLanguage />
-    //  </>
   );
 }
 
---
+++ HR_PLATFORM_UI/src/Components/Auth/LoginPage.jsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h4>LoginPage</h4>
+    </div>
+  );
+};
+
+export default LoginPage;
