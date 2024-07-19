05006219a9a830d4da0769614b8db72e8e6dad73
Catal1nPopa <misterco2002@gmail.com>
2024-07-19 11:26:55 +0300

    Revert "Added Navigation"
    This reverts commit 9e640c76e527580c21b4366f9834af256a7db351.

--- HR_PLATFORM_UI/src/Components/Auth/LoginPage.jsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-  return (
-    <div>
-      <h4>LoginPage</h4>
-    </div>
-  );
-};
-
-export default LoginPage;
--- HR_PLATFORM_UI/src/main.jsx
+++ HR_PLATFORM_UI/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import "./Translate/i18n.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
     <Suspense fallback={null} />
   </React.StrictMode>
 );
