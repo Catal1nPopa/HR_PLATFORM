9e640c76e527580c21b4366f9834af256a7db351
Catal1nPopa <misterco2002@gmail.com>
2024-07-19 10:34:57 +0300

    Added Navigation

---
+++ HR_PLATFORM_UI/src/Components/Main/MainPage.jsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+  return (
+    <div>
+      <h4>Home Page</h4>
+    </div>
+  );
+};
+
+export default MainPage;
--- HR_PLATFORM_UI/src/main.jsx
+++ HR_PLATFORM_UI/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import "./Translate/i18n.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
     <Suspense fallback={null} />
   </React.StrictMode>
 );
